e51434d92bc3f1ee2fd4308abac17d5acc86f195
tainafernandes <tainafefe@gmail.com>
2023-04-11 12:30:01 -0300

    Modifications of the login

--- routes.js
+++ routes.js
@@ -1,14 +1,16 @@
-const express = require("express");
+const express = require('express');
 const route = express.Router(); //trata as rotas
 
-const homeController = require("./src/controllers/homeController");
-const loginController = require("./src/controllers/loginController");
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
 
 //Rotas home
-route.get("/", homeController.index);
+route.get('/', homeController.index);
 
 //Rotas de Login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
 module.exports = route;
