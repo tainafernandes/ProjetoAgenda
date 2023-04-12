b0380dd929a7976e567e287dff50a40672ac164e
tainafernandes <tainafefe@gmail.com>
2023-04-12 13:11:55 -0300

    Finalization route contatos

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -51,11 +51,32 @@ class Contato {
 		};
 	}
 
-	async buscaPorId(id) {
+//Métodos estáticos
+
+	async buscaPorId (id) {
 		if (typeof id !== 'string') return;
-		const user = await ContatoModel.findById(id);
-		return user;
+		const contato = await ContatoModel.findById(id);
+		return contato;
 	}
-}
+
+	async buscaContatos () {
+		const contatos = await ContatoModel.find()
+			.sort({ criadoEm: -1 });
+		return contatos;
+	}
+
+	async edit(id){
+		if (typeof id !== 'string') return;
+		this.valida();
+		if(this.errors.length > 0) return;
+		this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new: true});
+	}
+
+	async delete(id) {
+		if (typeof id !== 'string') return;
+		const contato = await ContatoModel.findOneAndDelete(id);
+		return contato;
+	};
+};
 
 module.exports = Contato;
