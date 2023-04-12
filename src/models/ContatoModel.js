8604ccb43f1ed6f460ec7a7a8517b253d83308a8
tainafernandes <tainafefe@gmail.com>
2023-04-12 13:15:39 -0300

    Resolving bug

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -74,7 +74,7 @@ class Contato {
 
 	async delete(id) {
 		if (typeof id !== 'string') return;
-		const contato = await ContatoModel.findOneAndDelete(id);
+		const contato = await ContatoModel.findOneAndDelete({_id: id});
 		return contato;
 	};
 };
